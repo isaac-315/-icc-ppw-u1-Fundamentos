b053aeabcc3b524b4825b0fa461d706f07748fe2
isaac-315 <isaacalf.135@gmail.com>
2026-04-15 15:13:25 -0500

    Creado repositorio

---
+++ 01-sintaxis/js/app.js
@@ -0,0 +1,40 @@
+'use strict'
+const nombre = 'Isaac';
+const apellido = 'Mora';
+let ciclo = 5;
+const activo = true;
+
+const direccion = {
+    ciudad: 'Cuenca',
+    provincia: 'Azuay',
+}
+
+console.table({ nombre, apellido, ciclo, activo, direccion })
+
+const calcularPromedio = (notas) => {
+    const promedio = notas.reduce((a, b) => a + b, 0) / notas.length;
+}
+
+const esMayorDeEdad = (edad) => edad >= 18;
+
+const getSaludo = (nombre, hora) => {
+    if (hora < 12) {
+        return `Buenos días, ${nombre}`;
+    } else if (hora < 18) {
+        return `Buenas tardes, ${nombre}`;
+    } else {
+        return `Buenas noches, ${nombre}`;
+    }
+}
+
+const getSaludo2 = (nombre, hora) => {
+    hora < 12 ?
+        `Buenos días, ${nombre}` :
+        hora < 18 ? `Buenas tardes, ${nombre}` :
+            `Buenas noches, ${nombre}`;
+}
+
+//Mostrar en HTML
+document.getElementById('nombre').textContent = `${nombrePrincipal}`;
+document.getElementById('apellido').textContent = `${apellido}`;
+document.getElementById('ciclo').textContent = `${ciclo}`;
